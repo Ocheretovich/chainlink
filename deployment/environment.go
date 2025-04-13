b7bbaa0c40df5d5f9121a609e89f7fc49df133bf
Rodrigo <rodrigombsoares@gmail.com>
2025-04-13 12:25:45 -0300

    refactor: deploy chain with a single proposal

--- deployment/environment.go
+++ deployment/environment.go
@@ -175,6 +175,7 @@ func (e Environment) Clone() Environment {
 		DataStore:         ds.Seal(),
 		Chains:            e.Chains,
 		SolChains:         e.SolChains,
+		AptosChains:       e.AptosChains,
 		NodeIDs:           e.NodeIDs,
 		Offchain:          e.Offchain,
 		GetContext:        e.GetContext,
